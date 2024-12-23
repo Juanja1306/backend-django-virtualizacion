37031a5e66a1332439969dee95afb21a63c9d198
Juanja1306 <juanjamalovega@gmail.com>
2024-12-22 19:47:36 -0500

    Delete forms.py

--- backend_django/database/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms # type: ignore
-from .models import Imagen
-
-class ImagenForm(forms.ModelForm):
-    class Meta:
-        model = Imagen
-        fields = ['titulo', 'descripcion']
